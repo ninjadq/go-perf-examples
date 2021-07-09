a1c8d66daeefdcc5843ec07326a1a293020744e1
DQ <dengq@vmware.com>
2021-07-09 11:19:10 +0800

    init

---
+++ benchmark/string_concat/concat_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func BenchmarkPlusConcat(b *testing.B) {
+	var main string
+	for i := 0; i < b.N; i++ {
+		main = ""
+		main += "userid : " + "1"
+		main += "localtion : " + "ab"
+	}
+}
+
+func BenchmarkSprintf(b *testing.B) {
+	var main string
+	for i := 0; i < b.N; i++ {
+		main = ""
+		main += fmt.Sprintf("userid : %s", "1")
+		main += fmt.Sprintf("location : %s", "ab")
+	}
+}
